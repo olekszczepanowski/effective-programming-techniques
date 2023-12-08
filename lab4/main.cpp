c0a60bc31f0cbb929969c27022be67ac49d8d3de
olekszczepanowski <120830093+olekszczepanowski@users.noreply.github.com>
2023-12-08 17:34:32 +0100

    test

--- lab4/main.cpp
+++ lab4/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-
+#include "cmake-build-debug/CConsole.h"
+#include "cmake-build-debug/CNode.h"
+#include "cmake-build-debug/CTree.h"
 int main() {
-    std::cout << "Hello, Worldasdadasdasd!" << std::endl;
-    return 0;
+    CConsole* console = new CConsole();
+    console->menu();
+    delete console;
 }
