743e2883098446a220b85c4805fd9fbbba8d9505
olekszczepanowski <120830093+olekszczepanowski@users.noreply.github.com>
2023-12-08 17:34:20 +0100

    test

--- lab4/cmake-build-debug/CConsole.h
+++ lab4/cmake-build-debug/CConsole.h
@@ -6,9 +6,23 @@
 #define LAB4_CCONSOLE_H
 
 
+#include "CTree.h"
+using namespace std;
 class CConsole {
-
+private:
+    string formula;
+    CTree* tree;
+public:
+    CConsole();
+    void menu();
 };
+static const string JOIN = "join";
+static const string PRINT = "print";
+static const string COMP = "comp";
+static const string ENTER = "enter";
+static const string VARS = "vars";
+static const string QUIT = "quit";
+
 
 
 #endif //LAB4_CCONSOLE_H
