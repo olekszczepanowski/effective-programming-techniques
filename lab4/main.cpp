b0f9ca3a35d41adfc37d3e275defc76c2ac9e22f
olekszczepanowski <120830093+olekszczepanowski@users.noreply.github.com>
2023-12-18 16:09:48 +0100

    Delete lab4 directory

--- lab4/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "cmake-build-debug/CConsole.h"
-#include "cmake-build-debug/CNode.h"
-#include "cmake-build-debug/CTree.h"
-int main() {
-    CConsole* console = new CConsole();
-    console->menu();
-    delete console;
-}
